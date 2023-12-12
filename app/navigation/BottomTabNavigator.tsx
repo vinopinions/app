d8b639eedb0ea6f39f0e060817612f78ec79afda
oskar <ooskar@gmx.de>
2023-12-12 21:27:37 +0100

    fixing icons

--- app/navigation/BottomTabNavigator.tsx
+++ app/navigation/BottomTabNavigator.tsx
@@ -5,14 +5,46 @@ import FriendsScreen from '../screens/friends/FriendsScreen';
 import HomeScreen from '../screens/home/HomeScreen';
 import StoreScreen from '../screens/store/store-screen';
 import WinesStackScreen from '../screens/wines/WinesStackScreen';
+import {
+    AccountIcon,
+    AccountIconOutline,
+    FriendsIcon,
+    FriendsIconOutline,
+    HomeIcon,
+    HomeIconOutline,
+    StoreIcon,
+    StoreIconOutline,
+    WineIcon,
+    WineIconOutline
+} from '../utils/icons';
+
+const HomeRouteName = 'Home';
+const WinesStackRouteName = 'WinesStack';
+const StoresRouteName = 'Stores';
+const FriendsRouteName = 'Friends';
+const AccountRouteName = 'Account';
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => (
-    <BottomTab.Navigator screenOptions={{ headerShown: false }}>
-        <BottomTab.Screen name="Home" component={HomeScreen} options={{ tabBarLabel: 'Home', headerShown: false }} />
+    <BottomTab.Navigator
+        screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ size, color, focused }) => {
+                return getIconForScreen(route.name, size, color, focused);
+            }
+        })}
+    >
         <BottomTab.Screen
-            name="WinesStack"
+            name={HomeRouteName}
+            component={HomeScreen}
+            options={{
+                tabBarLabel: 'Home',
+                headerShown: false
+            }}
+        />
+        <BottomTab.Screen
+            name={WinesStackRouteName}
             component={WinesStackScreen}
             options={{
                 headerShown: false,
@@ -20,21 +52,21 @@ const BottomTabNavigator = () => (
             }}
         />
         <BottomTab.Screen
-            name="Stores"
+            name={StoresRouteName}
             component={StoreScreen}
             options={{
                 headerShown: false
             }}
         />
         <BottomTab.Screen
-            name="Friends"
+            name={FriendsRouteName}
             component={FriendsScreen}
             options={{
                 headerShown: false
             }}
         />
         <BottomTab.Screen
-            name="Account"
+            name={AccountRouteName}
             component={AccountDrawer}
             options={{
                 headerShown: false
@@ -44,3 +76,18 @@ const BottomTabNavigator = () => (
 );
 
 export default BottomTabNavigator;
+
+const getIconForScreen = (routeName: string, size: number, color: string, focused: boolean): React.JSX.Element => {
+    switch (routeName) {
+        case HomeRouteName:
+            return focused ? <HomeIcon size={size} color={color} /> : <HomeIconOutline size={size} color={color} />;
+        case WinesStackRouteName:
+            return focused ? <WineIcon size={size} color={color} /> : <WineIconOutline size={size} color={color} />;
+        case StoresRouteName:
+            return focused ? <StoreIcon size={size} color={color} /> : <StoreIconOutline size={size} color={color} />;
+        case FriendsRouteName:
+            return focused ? <FriendsIcon size={size} color={color} /> : <FriendsIconOutline size={size} color={color} />;
+        case AccountRouteName:
+            return focused ? <AccountIcon size={size} color={color} /> : <AccountIconOutline size={size} color={color} />;
+    }
+};
--- app/utils/icons.tsx
+++ app/utils/icons.tsx
@@ -1,20 +1,21 @@
-// https://fontawesome.com/v5/search
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { IconProps } from '@expo/vector-icons/build/createIconSet';
+// https://ionic.io/ionicons
 
-import { FontAwesome5 } from '@expo/vector-icons';
-import React from 'react';
+export const HomeIcon = (props: Partial<IconProps<'home'>>) => <Ionicons {...props} name="home" />;
+export const HomeIconOutline = (props: Partial<IconProps<'home-outline'>>) => <Ionicons {...props} name="home-outline" />;
 
-export const HomeIcon = props => <FontAwesome5 {...props} name="home" />;
+export const WineIcon = (props: Partial<IconProps<'wine'>>) => <Ionicons {...props} name="wine" />;
+export const WineIconOutline = (props: Partial<IconProps<'wine-outline'>>) => <Ionicons {...props} name="wine-outline" />;
 
-export const WineIcon = props => <FontAwesome5 {...props} name="wine-glass-alt" />;
+export const StoreIcon = (props: Partial<IconProps<'cart'>>) => <Ionicons {...props} name="cart" />;
+export const StoreIconOutline = (props: Partial<IconProps<'cart-outline'>>) => <Ionicons {...props} name="cart-outline" />;
 
-export const StoreIcon = props => <FontAwesome5 {...props} name="store-alt" />;
+export const FriendsIcon = (props: Partial<IconProps<'rocket'>>) => <Ionicons {...props} name="rocket" />;
+export const FriendsIconOutline = (props: Partial<IconProps<'rocket-outline'>>) => <Ionicons {...props} name="rocket-outline" />;
 
-export const FriendsIcon = props => <FontAwesome5 {...props} name="user-friends" />;
+export const AccountIcon = (props: Partial<IconProps<'person-circle'>>) => <Ionicons {...props} name="person-circle" />;
+export const AccountIconOutline = (props: Partial<IconProps<'person-circle-outline'>>) => <Ionicons {...props} name="person-circle-outline" />;
 
-export const AccountIcon = props => <FontAwesome5 {...props} name="user" />;
-
-export const EyeIcon = props => <FontAwesome5 {...props} name="eye" />;
-
-export const EyeSlashIcon = props => <FontAwesome5 {...props} name="eye-slash" />;
-
-export const PlusIcon = props => <FontAwesome5 {...props} name="plus" />;
+export const EyeIcon = (props: Partial<IconProps<'eye-outline'>>) => <Ionicons {...props} name="eye-outline" />;
+export const EyeSlashIcon = (props: Partial<IconProps<'eye-off-outline'>>) => <Ionicons {...props} name="eye-off-outline" />;
