24d3da181de23c1577e2bef601147256cb305719
tschokri <layla.shokri@gmx.de>
2024-01-02 23:17:07 +0100

    added search bar to wines screen

--- app/screens/wines/WinesScreen.tsx
+++ app/screens/wines/WinesScreen.tsx
@@ -6,11 +6,31 @@ import AddButton from '../../components/PlusButton';
 import WineCardList from '../../components/WineCardList';
 import { fetchWinesAsync } from '../../features/wines/winesSlice';
 import { AppDispatch, RootState } from '../../store/store';
+import SearchBar from '../utils/Searchbar';
+import Wine from '../../models/Wine';
 
 const WinesScreen = ({ navigation }) => {
     const [refreshing, setRefreshing] = useState(false);
     const dispatch: AppDispatch = useDispatch();
     const wines = useSelector((state: RootState) => state.wines.items);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [searchResults, setSearchResults] = useState<Wine[]>(wines);
+
+    const performSearch = () => {
+        if (searchQuery === '') setSearchResults(wines);
+        else {
+            const results = wines.filter(w => w.name.toLowerCase().includes(searchQuery.toLowerCase()));
+            setSearchResults(results);
+        }
+    };
+
+    const handleSearch = (query: string) => {
+        setSearchQuery(query);
+    };
+
+    useEffect(() => {
+        performSearch();
+    }, [searchQuery, wines]);
 
     const onRefresh = useCallback(async () => {
         setRefreshing(true);
@@ -27,7 +47,8 @@ const WinesScreen = ({ navigation }) => {
     }, []);
     return (
         <View style={styles.screen}>
-            <WineCardList refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />} wines={wines} />
+            <SearchBar searchQuery={searchQuery} handleSearch={handleSearch} />
+            <WineCardList refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />} wines={searchResults} />
             <AddButton onPress={() => onAddButtonPress()} style={styles.plusButton} />
         </View>
     );
