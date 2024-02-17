c1d21527a8c1b189e8db51f7ac40f8997d202a22
oskar <ooskar@gmx.de>
2024-02-17 10:56:54 +0100

    chore: add react-native-star-rating-widget

--- app/screens/rating/CreateRatingScreen.tsx
+++ app/screens/rating/CreateRatingScreen.tsx
@@ -1,69 +1,90 @@
-import { Button, Text, TextField, View } from 'react-native-ui-lib';
-import { CreateRatingScreenNavigationProp, CreateRatingScreenRouteProp } from '../wines/WinesStackScreen';
-import Wine from '../../models/Wine';
-import StarRating from 'react-native-star-rating-widget';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Keyboard, TouchableOpacity } from 'react-native';
-import Rating from '../../models/Rating';
-import { createWineRatingAsync } from '../../features/ratings/ratingsSlice';
+import StarRating from 'react-native-star-rating-widget';
+import { Button, Text, TextField, View } from 'react-native-ui-lib';
 import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../store/store';
-import React from 'react';
+import { createWineRatingAsync } from '../../features/ratings/ratingsSlice';
 import { fetchWinesAsync } from '../../features/wines/winesSlice';
+import Rating from '../../models/Rating';
+import Wine from '../../models/Wine';
+import { AppDispatch } from '../../store/store';
+import {
+  CreateRatingScreenNavigationProp,
+  CreateRatingScreenRouteProp,
+} from '../wines/WinesStackScreen';
 
 const CreateRatingScreen: React.FC<{
-    route: CreateRatingScreenRouteProp;
-    navigation: CreateRatingScreenNavigationProp;
+  route: CreateRatingScreenRouteProp;
+  navigation: CreateRatingScreenNavigationProp;
 }> = ({ route, navigation }): React.ReactElement => {
-    const dispatch: AppDispatch = useDispatch();
-    const wine: Wine = route.params.wine;
-    const [stars, setStars] = useState(0);
-    const [text, setText] = useState<string>();
+  const dispatch: AppDispatch = useDispatch();
+  const wine: Wine = route.params.wine;
+  const [stars, setStars] = useState(0);
+  const [text, setText] = useState<string>();
 
-    const onSubmitButtonPress = React.useCallback(() => {
-        const onSubmitButtonPressAsync = async () => {
-            const rating: Rating = { stars, text };
-            await dispatch(createWineRatingAsync({ wineId: wine.id, rating: rating }));
-            await dispatch(fetchWinesAsync());
-            navigation.goBack();
-        };
-        onSubmitButtonPressAsync();
-    }, [stars, text, wine, dispatch, navigation]);
+  const onSubmitButtonPress = React.useCallback(() => {
+    const onSubmitButtonPressAsync = async () => {
+      const rating: Rating = { stars, text };
+      await dispatch(
+        createWineRatingAsync({ wineId: wine.id, rating: rating }),
+      );
+      await dispatch(fetchWinesAsync());
+      navigation.goBack();
+    };
+    onSubmitButtonPressAsync();
+  }, [stars, text, wine, dispatch, navigation]);
 
-    return (
-        <TouchableOpacity onPress={Keyboard.dismiss} style={{ flex: 1 }} activeOpacity={1}>
-            <View flex>
-                <View>
-                    <Text text40 style={{ paddingBottom: 10, paddingTop: 20, paddingLeft: 10 }}>
-                        {wine.name}
-                    </Text>
-                </View>
-                <Text text50 style={{ marginTop: 15, marginLeft: 10 }}>
-                    Rating:
-                </Text>
-                <View>
-                    <StarRating
-                        style={{ paddingLeft: 5 }}
-                        rating={stars}
-                        maxStars={5}
-                        onChange={setStars}
-                        enableHalfStar={false}
-                    />
-                </View>
-                <View>
-                    <TextField
-                        multiline={true}
-                        style={{ borderWidth: 0.5, height: 100, fontSize: 20, margin: 10, marginTop: 5 }}
-                        value={text}
-                        onChangeText={setText}
-                    />
-                </View>
-                <View>
-                    <Button label="Submit rating" style={{ margin: 10 }} onPress={onSubmitButtonPress} />
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity
+      onPress={Keyboard.dismiss}
+      style={{ flex: 1 }}
+      activeOpacity={1}
+    >
+      <View flex>
+        <View>
+          <Text
+            text40
+            style={{ paddingBottom: 10, paddingTop: 20, paddingLeft: 10 }}
+          >
+            {wine.name}
+          </Text>
+        </View>
+        <Text text50 style={{ marginTop: 15, marginLeft: 10 }}>
+          Rating:
+        </Text>
+        <View>
+          <StarRating
+            style={{ paddingLeft: 5 }}
+            rating={stars}
+            maxStars={5}
+            onChange={setStars}
+            enableHalfStar={false}
+          />
+        </View>
+        <View>
+          <TextField
+            multiline={true}
+            style={{
+              borderWidth: 0.5,
+              height: 100,
+              fontSize: 20,
+              margin: 10,
+              marginTop: 5,
+            }}
+            value={text}
+            onChangeText={setText}
+          />
+        </View>
+        <View>
+          <Button
+            label="Submit rating"
+            style={{ margin: 10 }}
+            onPress={onSubmitButtonPress}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 export default CreateRatingScreen;
