e35486f2c3196b32b8d3014ca5476b12f42d804d
oskar <ooskar@gmx.de>
2024-02-18 21:33:02 +0100

    chore: fix get wines

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,9 +1,9 @@
 import { Credentials } from '../auth/AuthContext';
-import Rating from '../models/Rating';
 import {
   AUTH_ENDPOINT_URL,
   AUTH_LOGIN_ENDPOINT_URL,
   AUTH_SIGNUP_ENDPOINT_URL,
+  ID_URL_PARAMETER,
   RATINGS_ENDPOINT_URL,
   RATINGS_ID_ENDPOINT_URL,
   STORES_ENDPOINT_URL,
@@ -15,11 +15,11 @@ import {
   WINES_ID_ENDPOINT_URL,
   WINES_ID_RATINGS_ENDPOINT_URL,
 } from '../constants/UrlConstants';
+import Rating from '../models/Rating';
 import Store from '../models/Store';
 import Wine from '../models/Wine';
 import Winemaker from '../models/Winemaker';
 import { createDefaultAxiosInstance } from './utils';
-
 
 export const apiAuth = createDefaultAxiosInstance({
   baseURL: AUTH_ENDPOINT_URL,
@@ -42,27 +42,46 @@ export const login = (credentials: Credentials) =>
 export const signup = (credentials: Credentials) =>
   apiAuth.post(AUTH_SIGNUP_ENDPOINT_URL, credentials);
 
-export const apiWineRatings = createDefaultAxiosInstance({ baseURL: WINES_ENDPOINT_URL });
+export const apiWineRatings = createDefaultAxiosInstance({
+  baseURL: WINES_ENDPOINT_URL,
+});
 
-export const apiUsers = createDefaultAxiosInstance({ baseURL: USERS_ENDPOINT_URL });
+export const apiUsers = createDefaultAxiosInstance({
+  baseURL: USERS_ENDPOINT_URL,
+});
 
-export const apiRatings = createDefaultAxiosInstance({ baseURL: RATINGS_ENDPOINT_URL });
+export const apiRatings = createDefaultAxiosInstance({
+  baseURL: RATINGS_ENDPOINT_URL,
+});
 
 export const fetchCurrentUser = () => apiUsers.get(USERS_ME_ENDPOINT_URL);
 
-export const fetchWineById = (wineId: string) => apiWines.get(WINES_ID_ENDPOINT_URL);
+export const fetchWineById = (wineId: string) =>
+  apiWines.get(WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId));
 
+export const fetchWines = () => apiWines.get('');
 export const createWine = (wine: Wine) => apiWines.post('', wine);
-export const updateStoresForWine = (wineId: string, storeIds: string[]) => apiWines.put(WINES_ID_ENDPOINT_URL, { storeIds });
+export const updateStoresForWine = (wineId: string, storeIds: string[]) =>
+  apiWines.put(WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId), {
+    storeIds,
+  });
 
 export const fetchWinemakers = () => apiWinemakers.get('');
 export const createWinemaker = (winemaker: Winemaker) =>
   apiWinemakers.post('', winemaker);
 
 export const fetchStores = () => apiStores.get('/');
-export const fetchStoreById = (storeId: string) => apiStores.get(STORES_ID_ENDPOINT_URL);
+export const fetchStoreById = (storeId: string) =>
+  apiStores.get(STORES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, storeId));
 export const createStore = (store: Store) => apiStores.post('', store);
 
-export const createWineRating = (wineId: string, rating: Rating) => apiWineRatings.post(WINES_ID_RATINGS_ENDPOINT_URL, rating);
+export const createWineRating = (wineId: string, rating: Rating) =>
+  apiWineRatings.post(
+    WINES_ID_RATINGS_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId),
+    rating,
+  );
 
-export const deleteRating = (ratingId: string) => apiRatings.delete(RATINGS_ID_ENDPOINT_URL);
+export const deleteRating = (ratingId: string) =>
+  apiRatings.delete(
+    RATINGS_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, ratingId),
+  );
--- app/features/wines/winesSlice.ts
+++ app/features/wines/winesSlice.ts
@@ -1,90 +1,120 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import ApiResponseState from '../../api/ApiResponseState';
-import { createWine, fetchWineById, fetchWines, updateStoresForWine } from '../../api/api';
+import {
+  createWine,
+  fetchWineById,
+  fetchWines,
+  updateStoresForWine,
+} from '../../api/api';
 import Wine from '../../models/Wine';
 
 type WinesState = ApiResponseState<Wine[]>;
 
-export const fetchWinesAsync = createAsyncThunk<Wine[]>('wines/fetchWines', async () => {
+export const fetchWinesAsync = createAsyncThunk<Wine[]>(
+  'wines/fetchWines',
+  async () => {
     const response = await fetchWines();
     return response.data;
-});
-
-export const fetchWineByIdAsync = createAsyncThunk<Wine, string>('wines/fetchWineById', async (wineId: string) => {
-    const response = await fetchWineById(wineId);
-    return response.data;
-});
-
-export const createWineAsync = createAsyncThunk('wines/createWine', async (wine: Wine) => {
-    const response = await createWine(wine);
-    return response.data;
-});
-
-export const updateStoresForWineAsync = createAsyncThunk<Wine, { wineId: string; storeIds: string[] }>(
-    'wines/updateStoresForWine',
-    async ({ wineId, storeIds }) => {
-        const response = await updateStoresForWine(wineId, storeIds);
-        return response.data;
-    }
+  },
 );
 
+export const fetchWineByIdAsync = createAsyncThunk<Wine, string>(
+  'wines/fetchWineById',
+  async (wineId: string) => {
+    const response = await fetchWineById(wineId);
+    return response.data;
+  },
+);
+
+export const createWineAsync = createAsyncThunk(
+  'wines/createWine',
+  async (wine: Wine) => {
+    const response = await createWine(wine);
+    return response.data;
+  },
+);
+
+export const updateStoresForWineAsync = createAsyncThunk<
+  Wine,
+  { wineId: string; storeIds: string[] }
+>('wines/updateStoresForWine', async ({ wineId, storeIds }) => {
+  const response = await updateStoresForWine(wineId, storeIds);
+  return response.data;
+});
+
 const initialState: WinesState = {
-    data: [],
-    status: 'idle'
+  data: [],
+  status: 'idle',
 };
 
 const winesSlice = createSlice({
-    name: 'wines',
-    initialState: initialState as WinesState,
-    reducers: {},
-    extraReducers: builder => {
-        builder
-            .addCase(fetchWinesAsync.pending, state => {
-                state.status = 'loading';
-            })
-            .addCase(fetchWinesAsync.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                if (state.status == 'succeeded') state.data = action.payload;
-            })
-            .addCase(fetchWinesAsync.rejected, (state, action) => {
-                state.status = 'failed';
-                if (state.status == 'failed') state.error = action.error.message;
-            })
-            .addCase(fetchWineByIdAsync.pending, state => {
-                state.status = 'loading';
-            })
-            .addCase(fetchWineByIdAsync.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                if (state.status == 'succeeded') state.data = [action.payload];
-            })
-            .addCase(fetchWineByIdAsync.rejected, (state, action) => {
-                state.status = 'failed';
-                if (state.status == 'failed') state.error = action.error.message;
-            })
-            .addCase(updateStoresForWineAsync.pending, state => {
-                state.status = 'loading';
-            })
-            .addCase(updateStoresForWineAsync.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                if (state.status == 'succeeded') {
-                    const index = state.data.findIndex(wine => wine.id === action.payload.id);
-                    if (index !== -1) {
-                        state.data[index].stores = action.payload.stores;
-                    }
-                }
-            })
-            .addCase(updateStoresForWineAsync.rejected, (state, action) => {
-                state.status = 'failed';
-                if (state.status == 'failed') state.error = action.error.message;
-            })
-            .addCase(createWineAsync.fulfilled, (state, action) => {
-                if (state.status == 'succeeded') state.data.push(action.payload);
-            })
-            .addCase(createWineAsync.rejected, (state, action) => {
-                state.status = 'failed';
-                if (state.status == 'failed') state.error = action.error.message;
-            });
-    }
+  name: 'wines',
+  initialState: initialState as WinesState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWinesAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchWinesAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        if (state.status == 'succeeded') {
+          state.data = action.payload;
+        }
+      })
+      .addCase(fetchWinesAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        if (state.status == 'failed') {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(fetchWineByIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchWineByIdAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        if (state.status == 'succeeded') {
+          state.data = [action.payload];
+        }
+      })
+      .addCase(fetchWineByIdAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        if (state.status == 'failed') {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(updateStoresForWineAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(updateStoresForWineAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        if (state.status == 'succeeded') {
+          const index = state.data.findIndex(
+            (wine) => wine.id === action.payload.id,
+          );
+          if (index !== -1) {
+            state.data[index].stores = action.payload.stores;
+          }
+        }
+      })
+      .addCase(updateStoresForWineAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        if (state.status == 'failed') {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(createWineAsync.fulfilled, (state, action) => {
+        if (state.status == 'succeeded') {
+          state.data.push(action.payload);
+        }
+      })
+      .addCase(createWineAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        if (state.status == 'failed') {
+          state.error = action.error.message;
+        }
+      });
+  },
 });
 
 export default winesSlice.reducer;
