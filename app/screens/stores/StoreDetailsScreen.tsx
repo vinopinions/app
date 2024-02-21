5b22c88eeab68a67c21263e7a7e849b6a1d2a118
oskar <ooskar@gmx.de>
2024-02-21 18:22:20 +0100

    refactor: update last code to use new fixed eslint code

--- app/screens/stores/StoreDetailsScreen.tsx
+++ app/screens/stores/StoreDetailsScreen.tsx
@@ -3,15 +3,12 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from 'react-native-ui-lib';
 import WineCardList from '../../components/WineCardList';
 import Store from '../../models/Store';
-import Wine from '../../models/Wine';
 import { StoreDetailsScreenRouteProp } from './StoresStackScreen';
-import { getWinesForStore } from './utils/StoreUtils';
 
 const StoreDetailsScreen: React.FC<{ route: StoreDetailsScreenRouteProp }> = ({
   route,
 }): React.ReactElement => {
   const store: Store = route.params.store;
-  const winesAtStore: Wine[] = getWinesForStore(store);
   return (
     <View>
       <Text text40 style={styles.text}>
@@ -24,7 +21,7 @@ const StoreDetailsScreen: React.FC<{ route: StoreDetailsScreenRouteProp }> = ({
         Website: {store.url}
       </Text>
       <View>
-        <WineCardList wines={winesAtStore} />
+        <WineCardList wines={store.wines} />
       </View>
     </View>
   );
