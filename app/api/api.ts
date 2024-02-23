9c0061561c0832f5c36c88f5782e60ac66a11be3
oskar <ooskar@gmx.de>
2024-02-23 08:31:51 +0100

    refactor: add possibility to pass down axios options down to api calls

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestConfig } from 'axios';
 import { Credentials } from '../auth/AuthContext';
 import {
   AUTH_ENDPOINT_URL,
@@ -37,11 +38,6 @@ export const apiStores = createDefaultAxiosInstance({
   baseURL: STORES_ENDPOINT_URL,
 });
 
-export const login = (credentials: Credentials) =>
-  apiAuth.post(AUTH_LOGIN_ENDPOINT_URL, credentials);
-export const signup = (credentials: Credentials) =>
-  apiAuth.post(AUTH_SIGNUP_ENDPOINT_URL, credentials);
-
 export const apiWineRatings = createDefaultAxiosInstance({
   baseURL: WINES_ENDPOINT_URL,
 });
@@ -54,34 +50,73 @@ export const apiRatings = createDefaultAxiosInstance({
   baseURL: RATINGS_ENDPOINT_URL,
 });
 
-export const fetchCurrentUser = () => apiUsers.get(USERS_ME_ENDPOINT_URL);
+export const login = (credentials: Credentials, options?: AxiosRequestConfig) =>
+  apiAuth.post(AUTH_LOGIN_ENDPOINT_URL, credentials, options);
 
-export const fetchWineById = (wineId: string) =>
-  apiWines.get(WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId));
+export const signup = (
+  credentials: Credentials,
+  options?: AxiosRequestConfig,
+) => apiAuth.post(AUTH_SIGNUP_ENDPOINT_URL, credentials, options);
 
-export const fetchWines = () => apiWines.get('');
-export const createWine = (wine: WineDto) => apiWines.post('', wine);
-export const updateStoresForWine = (wineId: string, storeIds: string[]) =>
-  apiWines.put(WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId), {
-    storeIds,
-  });
+export const fetchCurrentUser = (options?: AxiosRequestConfig) =>
+  apiUsers.get(USERS_ME_ENDPOINT_URL, options);
 
-export const fetchWinemakers = () => apiWinemakers.get('');
-export const createWinemaker = (winemaker: Winemaker) =>
-  apiWinemakers.post('', winemaker);
+export const fetchWineById = (wineId: string, options?: AxiosRequestConfig) =>
+  apiWines.get(
+    WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId),
+    options,
+  );
 
-export const fetchStores = () => apiStores.get('/');
-export const fetchStoreById = (storeId: string) =>
-  apiStores.get(STORES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, storeId));
-export const createStore = (store: Store) => apiStores.post('', store);
+export const fetchWines = (options?: AxiosRequestConfig) =>
+  apiWines.get('', options);
 
-export const createWineRating = (wineId: string, rating: RatingDto) =>
+export const createWine = (wine: WineDto, options?: AxiosRequestConfig) =>
+  apiWines.post('', wine, options);
+
+export const updateStoresForWine = (
+  wineId: string,
+  storeIds: string[],
+  options?: AxiosRequestConfig,
+) =>
+  apiWines.put(
+    WINES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId),
+    { storeIds },
+    options,
+  );
+
+export const fetchWinemakers = (options?: AxiosRequestConfig) =>
+  apiWinemakers.get('', options);
+
+export const createWinemaker = (
+  winemaker: Winemaker,
+  options?: AxiosRequestConfig,
+) => apiWinemakers.post('', winemaker, options);
+
+export const fetchStores = (options?: AxiosRequestConfig) =>
+  apiStores.get('/', options);
+
+export const fetchStoreById = (storeId: string, options?: AxiosRequestConfig) =>
+  apiStores.get(
+    STORES_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, storeId),
+    options,
+  );
+
+export const createStore = (store: Store, options?: AxiosRequestConfig) =>
+  apiStores.post('', store, options);
+
+export const createWineRating = (
+  wineId: string,
+  rating: RatingDto,
+  options?: AxiosRequestConfig,
+) =>
   apiWineRatings.post(
     WINES_ID_RATINGS_ENDPOINT_URL.replace(ID_URL_PARAMETER, wineId),
     rating,
+    options,
   );
 
-export const deleteRating = (ratingId: string) =>
+export const deleteRating = (ratingId: string, options?: AxiosRequestConfig) =>
   apiRatings.delete(
     RATINGS_ID_ENDPOINT_URL.replace(ID_URL_PARAMETER, ratingId),
+    options,
   );
