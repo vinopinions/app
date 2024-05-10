215975b8b8741ffae77436466a752735caeb017a
oskar <ooskar@gmx.de>
2024-05-10 13:09:05 +0200

    chore: start new

--- app/App.tsx
+++ app/App.tsx
@@ -5,9 +5,8 @@ import auth from '@react-native-firebase/auth';
 import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from 'expo';
 import * as Localization from 'expo-localization';
-import * as Notifications from 'expo-notifications';
 import i18n from 'i18next';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { initReactI18next } from 'react-i18next';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -17,7 +16,6 @@ import { AuthProvider, useAuth } from './auth/AuthContext';
 import de from './locales/de.json';
 import en from './locales/en.json';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
-import { registerForPushNotificationsAsync } from './notifications/Notification';
 import LoginScreen from './screens/login/LoginScreen';
 import SignupScreen from './screens/login/SignupScreen';
 import { store } from './store/store';
@@ -42,41 +40,6 @@ if (FIREBASE_AUTH_EMULATOR_URL) {
 }
 
 const App = () => {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] =
-    useState<Notifications.Notification>();
-  const notificationListener = useRef<Notifications.Subscription>();
-  const responseListener = useRef<Notifications.Subscription>();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token),
-    );
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener(
-        (newNotification: Notifications.Notification) => {
-          setNotification(newNotification);
-        },
-      );
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
-
-    return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current,
-      );
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log({ expoPushToken, notification });
-  }, [expoPushToken, notification]);
-
   return (
     <Provider store={store}>
       <AuthProvider>
